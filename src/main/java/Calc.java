0cbb9415598ad387403d5af9d047014b4aade482
yosi475 <yosi475@naver.com>
2026-07-14 10:15:40 +0900

    t1, t2, t3, t4

---
+++ src/main/java/Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+    public static int run(String cal) {
+        int result = 0;
+        String[] splitStr = cal.split(" ");
+        int value1 = Integer.parseInt(splitStr[0]);
+        int value2 = Integer.parseInt(splitStr[2]);
+        System.out.println(value1);
+        System.out.println(value2);
+        System.out.println(splitStr[1]);
+        if(splitStr[1].equals("+")){
+            result = value1 + value2;
+        }
+        return result;
+    }
+}
