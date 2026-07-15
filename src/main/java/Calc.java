a03329589ee8d58fb7eaef5a4405f37e5d4b0df1
yosi475 <yosi475@naver.com>
2026-07-15 11:14:50 +0900

    t23, t24, t25, t26, t27, t28

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -25,27 +25,55 @@ public class Calc {
                 break; // 껍데기 괄호가 아니면 탈출!
             }
         }
+
+        int depth = 0;
         for(int i = trimStr.length() - 1; i >= 0; i--){
-            if(trimStr.charAt(i) == '+'){
-                return run(trimStr.substring(0,i)) + run(trimStr.substring(i+1));
-            }
-            if(trimStr.charAt(i) == '-'){
-                if(i == 0 || isOperator(trimStr.charAt(i - 1))) {
+            char c = trimStr.charAt(i);
+
+            if(c == ')')depth++;
+            else if(c == '(')depth--;
+
+            if(depth == 0) {
+                if (c == '+') {
+                    if (i > 0 && isOperator(trimStr.charAt(i - 1))) {
+                        continue;
+                    }
+                    return run(trimStr.substring(0, i)) + run(trimStr.substring(i + 1));
+                }
+                if (c == '-') {
+                    if (i > 0 && (Character.isDigit(trimStr.charAt(i - 1)) || trimStr.charAt(i - 1) == ')')) {
+                        return run(trimStr.substring(0, i)) - run(trimStr.substring(i + 1));
+
+                    }
                     continue;
                 }
-                return run(trimStr.substring(0,i)) - run(trimStr.substring(i+1));
             }
         }
-        for (int i = trimStr.length() - 1; i >= 0; i--) {
-            if (trimStr.charAt(i) == '*') {
-                return run(trimStr.substring(0, i)) * run(trimStr.substring(i + 1));
+
+        depth = 0;
+
+        for(int i = trimStr.length() - 1; i >= 0; i--) {
+            char c = trimStr.charAt(i);
+
+            if (c == ')') depth++;
+            else if (c == '(') depth--;
+
+            if (depth == 0) {
+                    if (c == '*') {
+                        return run(trimStr.substring(0, i)) * run(trimStr.substring(i + 1));
+                    }
+                    if (c == '/') {
+                        return run(trimStr.substring(0, i)) / run(trimStr.substring(i + 1));
+                    }
+                }
             }
-            if (trimStr.charAt(i) == '/') {
-                return run(trimStr.substring(0, i)) / run(trimStr.substring(i + 1));
+
+                if (trimStr.startsWith("-")) {
+                    return -1 * run(trimStr.substring(1)); // 마이너스를 뗀 뒷부분만 다시 재귀로 구해서 곱해줍니다!
+                }
+                return Integer.parseInt(trimStr);
             }
-        }
-        return Integer.parseInt(trimStr);
-    }
+
     public static boolean isOperator(char c){
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
