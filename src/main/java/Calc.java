499b45b9ed97b2dfe19e2136553a97220f9ce9a5
yosi475 <yosi475@naver.com>
2026-07-15 09:36:42 +0900

    t13, t14, t15, t16

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,14 +1,31 @@
 public class Calc {
     public static int run(String cal) {
-        String trimStr = cal.trim();
+        if(cal == null || cal.trim().isEmpty()){
+            return 0;
+        }
+        String trimStr = cal.replace(" ", "");
         for(int i = trimStr.length() - 1; i >= 0; i--){
             if(trimStr.charAt(i) == '+'){
                 return run(trimStr.substring(0,i)) + run(trimStr.substring(i+1));
             }
             if(trimStr.charAt(i) == '-'){
+                if(i == 0 || isOperator(trimStr.charAt(i - 1))) {
+                    continue;
+                }
                 return run(trimStr.substring(0,i)) - run(trimStr.substring(i+1));
             }
         }
+        for (int i = trimStr.length() - 1; i >= 0; i--) {
+            if (trimStr.charAt(i) == '*') {
+                return run(trimStr.substring(0, i)) * run(trimStr.substring(i + 1));
+            }
+            if (trimStr.charAt(i) == '/') {
+                return run(trimStr.substring(0, i)) / run(trimStr.substring(i + 1));
+            }
+        }
         return Integer.parseInt(trimStr);
+    }
+    public static boolean isOperator(char c){
+        return c == '+' || c == '-' || c == '*' || c == '/';
     }
 }
